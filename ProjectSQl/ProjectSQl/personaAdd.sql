630e0d22b19eb2d6059f2243e9d0cafd5ce9e243
Martin Cruz M <codemcm@hotmail.com>
2021-06-30 18:57:03 -0600

    mas cambios

--- ProjectSQl/ProjectSQl/personaAdd.sql
+++ ProjectSQl/ProjectSQl/personaAdd.sql
@@ -1,6 +1,15 @@
 ﻿--CREATE STORE PROCEDURE FOR Insert
 
+/*
+ La instrucción SET solo se puede usar en variable a la vez. 
+ Esto puede volverse engorroso si necesita asignar valores de atributos múltiples. 
+ Pero es importante indicar que usted, sí necesita usar varias declaraciones de conjunto. 
+*/
 CREATE PROCEDURE personaAdd(
 		@userName VARCHAR(50),
 		@password VARCHAR(50)
+
+AS
+BEGIN 
+    DECLARE @PersonaId
 );
