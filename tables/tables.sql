a7854837a0c5365484587ba811f7906114cfdc82
Martin Cruz M <codemcm@hotmail.com>
2021-07-01 18:33:08 -0600

    script tables.sql

--- tables/tables.sql
+++ tables/tables.sql
@@ -1,15 +1,16 @@
+--DROP TABLE useruc
 CREATE TABLE useruc(
     useruc_id INT IDENTITY(1,1) PRIMARY KEY,
     user_name VARCHAR(50),
     password VARCHAR(50),
     last_access DATETIME,
-    active BIT,
+    active BIT DEFAULT 1,
     created_by INT,
-    created_date DATETIME,
-    update_date DATETIME,
+    created_date DATETIME DEFAULT getdate(),
+    update_date DATETIME DEFAULT getdate(),
     updated_by INT
 );
-
+--DROP TABLE client
 CREATE TABLE client(
     client_id INT IDENTITY(1,1) PRIMARY KEY,
     name VARCHAR(50),
@@ -17,56 +18,61 @@ CREATE TABLE client(
     cell VARCHAR(10),
     email VARCHAR(50),
     observations VARCHAR(300),
-    active BIT,
+    active BIT DEFAULT 1,
     created_by INT,
-    created_date DATETIME,
-    updated_by INT,
-    update_date DATETIME
+    created_date DATETIME DEFAULT getdate(),
+    update_date DATETIME DEFAULT getdate(),
+    updated_by INT
 );
-
+--DROP TABLE raffle
 CREATE TABLE raffle(
     raffle_id INT IDENTITY(1,1) PRIMARY KEY,
     description VARCHAR(50),
     start_date DATE,
     raffle_date DATE,
-    active BIT,
+    active BIT DEFAULT 1,
     created_by INT,
-    created_date DATETIME,
-    updated_by INT,
-    update_date DATETIME
+    created_date DATETIME DEFAULT getdate(),
+    update_date DATETIME DEFAULT getdate(),
+    updated_by INT
 );
 
-
+--DROP TABLE ticket
 CREATE TABLE ticket(
     ticket_id INT IDENTITY(1,1) PRIMARY KEY,
     raffle_id INT,
     client_id INT,
-    active BIT,
+    active BIT DEFAULT 1,
     created_by INT,
-    created_date DATETIME,
-    updated_by INT,
-    update_date DATETIME
+    created_date DATETIME DEFAULT getdate(),
+    update_date DATETIME DEFAULT getdate(),
+    updated_by INT
+    FOREIGN KEY (client_id) REFERENCES  client(client_id),
+    FOREIGN KEY (raffle_id) REFERENCES  raffle(raffle_id),
 );
-
+--DROP TABLE prize
 CREATE TABLE prize(
     prize_id INT,
     raffle_id INT,
     product VARCHAR(100),
-    active BIT,
+    active BIT DEFAULT 1,
     created_by INT,
-    created_date DATETIME,
-    updated_by INT,
-    update_date DATETIME
+    created_date DATETIME DEFAULT getdate(),
+    update_date DATETIME DEFAULT getdate(),
+    updated_by INT
+    FOREIGN KEY (raffle_id) REFERENCES  raffle(raffle_id)
 );
 
+--DROP TABLE winner
 CREATE TABLE winner(
     winner_id INT IDENTITY(1,1) PRIMARY KEY,
     ticket_id INT,
     prize_id INT,
-    active BIT,
+    active BIT DEFAULT 1,
     created_by INT,
-    created_date DATETIME,
-    updated_by INT,
-    update_date DATETIME
+    created_date DATETIME DEFAULT getdate(),
+    update_date DATETIME DEFAULT getdate(),
+    updated_by INT
+    FOREIGN KEY (ticket_id) REFERENCES  ticket(ticket_id)
 );
 
