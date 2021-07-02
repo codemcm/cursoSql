124295d0d0896386e519affb4128025c37e2c882
Martin Cruz M <codemcm@hotmail.com>
2021-07-01 19:03:33 -0600

    list user, need fix

---
+++ storeProcedure/listUsers.sql
@@ -0,0 +1,22 @@
+--CONSULTAR USUARIO
+
+-- =============================================  
+-- Author:      Martin Cruz Medinilla
+-- Create date: 19-Mayo-2017  
+-- Description: Return user records  
+-- =============================================  
+CREATE PROCEDURE usuarioSeleccionar  
+    -- Add the parameters for the stored procedure here  
+    @CityName nvarchar(30)  
+      
+AS  
+BEGIN  
+    -- SET NOCOUNT ON added to prevent extra result sets from  
+    -- interfering with SELECT statements.  
+    SET NOCOUNT ON;  
+  
+    Select * From tblMembers   
+    where MemberCity like '%'+@CityName+'%'  
+          
+END  
+GO  
