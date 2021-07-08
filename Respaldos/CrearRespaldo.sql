b92b819e5d66d932a4995af1ac6c569d933458af
Martin Cruz M <codemcm@hotmail.com>
2021-07-08 13:16:33 -0600

    crear carpetas con sql

--- Respaldos/CrearRespaldo.sql
+++ Respaldos/CrearRespaldo.sql
@@ -22,3 +22,24 @@ RECONFIGURE
 --llamar exe desde sql
 
 Exec xp_cmdshell "D:\appDate\EscribeArchivoFecha.exe" 
+
+
+--CREAR SCRIPT PARA RESPALDO CON NOMBRE DIRECTORIO FECHA
+
+declare @strNombreBackup varchar(100);
+declare @strNumSem varchar(5); 
+set @strNumSem = CAST(DATEPART(WEEK,GETDATE()) as varchar);
+set @strNombreBackup = 'D:\RespaldoBD\'+@strNumSem+'\Full-'+'.bak';
+backup database [testt]
+	TO DISK = @strNombreBackup
+	with format,
+	name = 'Full '+cast(getdate() as varchar),
+	compression;
+
+
+--CREAR CARPETAS USANDO SQL SERVER
+
+declare @MD varchar(100), @wk_no varchar(100)
+set @wk_no = 'D:\Respaldos\' + Convert(VarChar(4),DatePart(Year,GetDate()))+Right('0'+Convert(VarChar(2),DatePart(Month,GetDate())),2)+Right('0'+Convert(VarChar(2),DatePart(Day,GetDate( ))),2)
+SET @MD = ' mkdir ' + @wk_no
+EXEC xp_cmdshell @MD, no_output
