bc76b439281ca86b74ceeb1eea76c902ff9554f5
Martin Cruz M <codemcm@hotmail.com>
2021-07-16 13:09:23 -0600

    borrado logico

--- cloverStores/userLogicDelete.sql
+++ cloverStores/userLogicDelete.sql
@@ -1,21 +1,17 @@
---Name: userUpdate_sp
+--Name: userLogicDelete_sp
 -- =============================================  
 -- Author:      Martin Cruz Medinilla
 -- Create date: july 16 2021  
--- Description: Update an user
+-- Description: Logic Delete of user
 -- =============================================  
-CREATE PROCEDURE userUpdate_sp
+CREATE PROCEDURE userLogicDelete_sp
     @usuario_id INT,
-    @username VARCHAR(50),
-    @password VARCHAR(32),
     @Responsable INT
 AS
 BEGIN
     UPDATE useruc SET 
-        user_name= @username,
-        password= @password,
-        update_date= GETDATE(),
-        updated_by= @Responsable
+        active=0,
+        updated_by=@Responsable
     WHERE useruc_id= @usuario_id
     SELECT @@ROWCOUNT AS registrosAfectados
 END
